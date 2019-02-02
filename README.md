9587d4120941aa3b6c45a332b51d32c6475912d6
Prakash Pandey <theprakashchandra@gmail.com>
2019-02-02 22:19:45 +0530

    created basic project structure, implemented a basic http server

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fake-server
+
 A fake server for developing and testing client (web, mobile, console) applications. 
---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to fake-server"))
+}
+
+func superAdminHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Define endpoints and their responses"))
+}
+
+func createMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", rootHandler)
+	mux.HandleFunc("superAdmin", superAdminHandler)
+	return mux
+}
+
+func main() {
+	mux := createMux()
+	log.Fatal(http.ListenAndServe(":8284", mux))
+}
